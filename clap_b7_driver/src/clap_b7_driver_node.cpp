38df080617a76427e2567751e8b74183b7e57b8d
saidbur7765 <saidnur.caliskan@std.yildiz.edu.tr>
2024-04-04 17:25:07 +0300

    add rclcpp spin_some

--- clap_b7_driver/src/clap_b7_driver_node.cpp
+++ clap_b7_driver/src/clap_b7_driver_node.cpp
@@ -7,8 +7,11 @@ int main(int argc, char ** argv)
     rclcpp::init(argc, argv);
     auto node = std::make_shared<clap_b7::ClapB7Driver>(rclcpp::NodeOptions());
     rclcpp::Rate rate(200); // it has to be update minimum 200Hz
+    
+
     while(rclcpp::ok()){
         node->Update();
+        rclcpp::spin_some(node);
         rate.sleep();
     }
     rclcpp::shutdown();
